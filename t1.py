a124af1b36e3cb25a2b5671fb827b945566fee22
kiiiiiiit <s@dedov.ws>
2023-01-16 23:18:57 +0300

    t1 comments and modified

--- t1.py
+++ t1.py
@@ -1,27 +1,36 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
+import time
 
-blocks = [1,2,3,4,8,9,13,41]
+def sand_to_air(x):
+    if x in blocks:
+        x = 0
+    return x
+
+blocks = [1,2,3,4,8,9,12,13,24]
 x,y,z = pos = mc.player.getTilePos()
 # x,y,z = pos = 0,0,0
 print("Position is: ", pos)
+y=0
+lenght = int(input("Длина: "))
+width  = int(input("Ширина: "))
+depth  = int(input("Высота: "))
 
-delta = 2
-delta_x = x+delta
-delta_y = y+delta
-delta_z = z+delta
+delta_x = x+lenght
+delta_y = y+depth
+delta_z = z+width
 block = 0
 m = 0
 
 now = list(mc.getBlocks(pos, delta_x-1, delta_y-1, delta_z-1))
-# # print(list(now))
-for a in range(x,delta_x):
-    for b in range(y,delta_y):
-         for c in range(z,delta_z):
-            boom = a,b,c
-            target = now[m]
-            print("Найден: x:{},y:{},z:{}, получен:{}".format(a,b,c, target))
-            if target in blocks:
-                print("BOOM: x:{},y:{},z:{}, поставлен:{}".format(a,b,c, block))
-                mc.setBlock(boom, block)
+# print(now)
+new = list(map(sand_to_air, now))
+# print(new)
+time.sleep(5)
+for dy in range(y,delta_y):
+    for dx in range(x,delta_x):
+         for dz in range(z,delta_z):
+            boom = dx,dy,dz
+            target = new[m]
+            mc.setBlock(boom, target)
             m += 1
