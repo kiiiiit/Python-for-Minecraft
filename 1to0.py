b7a207635f6cd0687a781cc3c3a89ffc82aa5be4
kiiiiiiit <s@dedov.ws>
2023-01-16 15:37:55 +0300

    1to0 comments and modified

--- 1to0.py
+++ 1to0.py
@@ -1,25 +1,28 @@
+# Расчистка поверхности от инертных блоков (земля, гравий, камень и т.п.)
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x,y,z = pos = mc.player.getTilePos()
-y = 0
+x,y,z = pos = mc.player.getTilePos() #Получаем позицию игрока
+y = 0 # Выставляем уровень в 0
 print("Position is: ", pos)
-blocks = [1,2,3,4,13]
+
+blocks = [1,2,3,4,8,9,13] # Перечень типов блоков, что нужно убирать
 # blocks = [5,14,15,16,17,(17,1),(17,2),(17,3),21,37,38,39,40,56,73,129]
+
 lenght = int(input("Длина: "))
 width = int(input("Ширина: "))
-y = int(input("Глубина (от 0): "))
+y = int(input("Глубина начала ниже 0: "))
 
 for n in range(lenght):
-   dx = x+n
+   dx = x+n # Высчитываем Х
    for i in range(width):
-      dz = z+i
-      y_hi = mc.getHeight(dx,dz)
-      print("higest Y: x:{},y:{},z:{}".format(dx,y_hi,dz))
-      for j in range(y, y_hi+1):
-         boom = dx,j,dz
-         print("BOOM: x:{},y:{},z:{}".format(dx,j,dz))
-         target = mc.getBlock(boom)
+      dz = z+i # Высчитываем Z
+      y_hi = mc.getHeight(dx,dz) # Находим максимальную высоту в данной точке
+      # print("higest Y: x:{},y:{},z:{}".format(dx,y_hi,dz))
+      for dy in range(y, y_hi+1):
+         boom = dx,dy,dz # Вычисляем блок для анализа
+         # print("BOOM: x:{},y:{},z:{}".format(dx,dy,dz))
+         target = mc.getBlock(boom) # Получаем тип блока
          # if target not in blocks:
          if target in blocks:
-            mc.setBlock(boom, 0)
+            mc.setBlock(boom, 0) # Заполняем ненужные блоки "воздухом"
